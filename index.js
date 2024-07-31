f5ff4e16e8c5f00d42284854b8ee8a8fb70d10db
Swarit Choudhari <68472469+SwiftyProgrammer690@users.noreply.github.com>
2024-07-31 23:06:23 +0000

    failed attempt to try to animate scroll with js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-//alert("This is a mock website made by Swarit Choudhari for practice! If this is resembling any real company, it is PURELY UNINTENTIONAL...")
+
+
+// Animations
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show')
+        }
+    });
+});
+
+const hiddenItems = document.querySelectorAll('.hidden');
+hiddenItems.forEach((item) => observer.observe(item));
