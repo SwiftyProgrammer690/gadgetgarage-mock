70bf13d4e2b8bc5dd22a74ba18aefab530f4cbe9
Swarit Choudhari <68472469+SwiftyProgrammer690@users.noreply.github.com>
2024-08-01 14:30:43 +0000

    animating is hard...

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 
 
 // Animations
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-        } else {
-            entry.target.classList.remove('show')
-        }
-    });
-});
+document.addEventListener('DOMContentLoaded', function () {
+	const links = document.querySelectorAll('nav a');
+	const sections = document.querySelectorAll('.section');
 
-const hiddenItems = document.querySelectorAll('.hidden');
-hiddenItems.forEach((item) => observer.observe(item));
+	for (const link of links) {
+		link.addEventListener('click', function (event) {
+			event.preventDefault();
+			const targetId = this.getAttribute('href').substring(1);
+			const targetElement = document.getElementById(targetId);
+
+			window.scrollTo({
+				top: targetElement.offsetTop,
+				behavior: 'smooth'
+			});
+		});
+	}
+
+	const observer = new IntersectionObserver((entries) => {
+		entries.forEach(entry => {
+			if (entry.isIntersecting) {
+				entry.target.classList.add('show');
+			}
+		});
+	}, {
+		threshold: 0.1
+	});
+
+	sections.forEach(section => {
+		observer.observe(section);
+	});
+});
